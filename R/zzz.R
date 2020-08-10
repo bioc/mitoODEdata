e2377a3580e26ee726383282721ce8249c4999cf
Mike Smith <grimbough@gmail.com>
2020-08-10 12:05:28 +0200

    Remove deprecation message in release version

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,3 @@
 .onLoad <- function(...) {
   loadMitocheck()
 }
-.onAttach <- function(libname, pkgname) {
-    msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.13")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
-}
